1c14b4952fdfc142daf874e9444e729e1eaed6ea
4080220 <4080220@alu.murciaeduca.es>
2025-01-17 20:34:29 +0100

    Delete Menú principal directory

--- Menú principal/scenes/Screen_01.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var change_scene = preload(res://scenes/scene_02.tscn)
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(change_scene)
-	pass # Replace with function body.
