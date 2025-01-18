7ee24458a5c7a0284b13f2330f571c50739396dd
4080220 <4080220@alu.murciaeduca.es>
2025-01-18 15:29:59 +0100

    Add files via upload

---
+++ menu/Scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_jugar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_instrucciones_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().quit()
