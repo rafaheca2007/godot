b72b3a9532d89d9525900b5ce99b4357b33172d5
4080220 <4080220@alu.murciaeduca.es>
2025-01-24 12:54:12 +0100

    Delete menu- directory

--- menu-/scripts/instrucciones.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_salir_de_las_instrucciones_pressed() -> void:
-	get_tree(). change_scene_to_file("res://scenes/menu.tscn")
--- menu-/scripts/jugar.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-
-
-
-func _on_volver_pressed() -> void:
-	get_tree(). change_scene_to_file("res://scenes/menu.tscn")
