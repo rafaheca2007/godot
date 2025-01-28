ff60162003b7cd00505797296acc8c3b6dcee95e
mbappe <4080220@alu.murciaeduca.es>
2025-01-28 01:47:54 +0100

    Add files via upload

---
+++ menu-principal/Scripts/creditos.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+
+
+func _on_regresar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
---
+++ menu-principal/Scripts/instrucciones.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+
+
+
+func _on_atras_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
