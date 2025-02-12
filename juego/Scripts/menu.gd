b660bcc20fb1aa859928276a3859f69776c7f0f6
mbappe <4080220@alu.murciaeduca.es>
2025-02-12 13:32:01 +0100

    Add files via upload

---
+++ juego/Scripts/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+@onready var audio_stream_player: AudioStreamPlayer = $VBoxContainer/SALIR/AudioStreamPlayer
+
+
+
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/juego.tscn")
+
+
+func _on_instrucciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/instrucciones.tscn")
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/creditos.tscn")
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_salir_mouse_entered() -> void:
+	audio_stream_player.play()
