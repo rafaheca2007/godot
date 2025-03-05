5651bc4f5e4948fe8bfb3473dac952fd431b8414
mbappe <4080220@alu.murciaeduca.es>
2025-03-05 18:52:46 +0100

    Delete juego directory

--- juego/Scripts/menu_pausa.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-
-
-
-func _on_pausa_mouse_entered() -> void:
-	get_tree().change_scene_to_file("res://Escenas/juego.tscn")
--- juego/Scripts/salir.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_salir_pressed() -> void:
-	get_tree(). change_scene_to_file("res://Escenas/menu.tscn")
